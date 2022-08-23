783c8c582918f46f7b7658e6bd357530e7a910bd
AndyRCR <andycr321@gmail.com>
2022-08-23 05:32:25 -0500

    Blast bug test v2

--- src/components/BlastText/BlastText.jsx
+++ src/components/BlastText/BlastText.jsx
@@ -6,68 +6,43 @@ import Blast from 'blast-vanilla'
 const BlastText = ({ text }) => {
 
     useEffect(() => {
-        return () => {
-            new Blast('.blast-root', {
-                returnGenerated: true,
-                delimiter: 'character',
-                search: false,
-                customClass: '',
-                aria: true,
-                debug: false,
-                name: 'blast'
-            })
+        new Blast('.blast-root', {
+            returnGenerated: true,
+            delimiter: 'character',
+            search: false,
+            customClass: '',
+            aria: true,
+            debug: false,
+            name: 'blast'
+        })
 
-            let a = 0;
-            $(".home-page .blast").each(function () {
-                if (a === 300) {
-                    a = 400;
-                }
+        let a = 0;
+        $(".home-page .blast").each(function () {
+            if (a === 300) {
+                a = 400;
+            }
 
-                if (a === 1200) {
-                    a = 1400;
-                }
+            if (a === 1200) {
+                a = 1400;
+            }
 
-                var el = $(this);
-
-                setTimeout(function () {
-                    el.addClass("animated bounceIn");
-                }, a);
-
-                if (a < 1200) {
-                    a = a + 50;
-                } else {
-                    a = a + 35;
-                }
-            });
-            setTimeout(function () {
-                $(".home-page .blast").removeClass("animated bounceIn");
-                $(".home-page .blast").css("opacity", 1);
-
-                $(".home-page .blast").mouseenter(function () {
-                    var el = $(this);
-
-                    $(this).addClass("animated rubberBand");
-                    $(this).one(
-                        "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
-                        function () {
-                            el.removeClass("animated rubberBand");
-                        }
-                    );
-                });
-            }, 3000);
+            var el = $(this);
 
             setTimeout(function () {
-                $(".home-page .flat-button").addClass("animated bounceIn");
-                $(".home-page .flat-button").one(
-                    "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
-                    function () {
-                        $(".home-page .flat-button").removeClass("animated bounceIn");
-                        $(".home-page .flat-button").css("opacity", 1);
-                    }
-                );
-            }, 2000);
+                el.addClass("animated bounceIn");
+            }, a);
 
-            $(".home-page .flat-button").mouseenter(function () {
+            if (a < 1200) {
+                a = a + 50;
+            } else {
+                a = a + 35;
+            }
+        });
+        setTimeout(function () {
+            $(".home-page .blast").removeClass("animated bounceIn");
+            $(".home-page .blast").css("opacity", 1);
+
+            $(".home-page .blast").mouseenter(function () {
                 var el = $(this);
 
                 $(this).addClass("animated rubberBand");
@@ -78,7 +53,30 @@ const BlastText = ({ text }) => {
                     }
                 );
             });
-        }
+        }, 3000);
+
+        setTimeout(function () {
+            $(".home-page .flat-button").addClass("animated bounceIn");
+            $(".home-page .flat-button").one(
+                "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+                function () {
+                    $(".home-page .flat-button").removeClass("animated bounceIn");
+                    $(".home-page .flat-button").css("opacity", 1);
+                }
+            );
+        }, 2000);
+
+        $(".home-page .flat-button").mouseenter(function () {
+            var el = $(this);
+
+            $(this).addClass("animated rubberBand");
+            $(this).one(
+                "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+                function () {
+                    el.removeClass("animated rubberBand");
+                }
+            );
+        });
     }, [])
 
 
@@ -86,19 +84,19 @@ const BlastText = ({ text }) => {
         <>
             {text === 'main' && (
                 <h1 aria-label={text} className="blast-root">
-                    Hi! I'm Andy Canales,<br />a web developer
+                    Hi! I'm Andy Canales,<br/>a web developer
                 </h1>
             )}
 
             {text === 'about' && (
                 <h1 aria-label={text} className="blast-root">
-                    A little<br />about me...
+                    A little<br/>about me...
                 </h1>
             )}
 
             {text === 'skills' && (
                 <h1 aria-label={text} className="blast-root">
-                    Skills and<br />experience
+                    Skills and<br/>experience
                 </h1>
             )}
 
@@ -114,7 +112,7 @@ const BlastText = ({ text }) => {
                 </h1>
             )}
         </>
-
+        
     )
 }
 
