0cc7c4b5a2c8f84f34ddc28b8e5b7d23e5a822c4
AndyRCR <andycr321@gmail.com>
2022-09-29 15:19:22 -0500

    Modificacion loader v2

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Outlet } from 'react-router-dom'
+import { GlobalContext } from '../context/GlobalStateContext'
 import CloseButton from './CloseButton/CloseButton'
 import Loader from './Loader/Loader'
 import Navbar from './Navbar/Navbar'
 
 const Layout = () => {
+
+  const { loaderState } = useContext(GlobalContext)
+
   return (
-    <div style={{height: '100vh'}}>
+    <div style={{height: '100vh', overflowY: loaderState === 1 ? 'hidden' : 'visible'}}>
         <CloseButton/>
         <Loader/>
         <Navbar/>
