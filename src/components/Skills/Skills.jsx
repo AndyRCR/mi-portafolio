4fdd182b711330ae6ad7d4e5575c6c8796c0c3d3
AndyRCR <andycr321@gmail.com>
2022-10-05 16:45:45 -0500

    Skills modify

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -26,9 +26,11 @@ const Skills = () => {
                             Since I started in the world of web programming 4 years ago I find
                             myself refining my skills.
                             <br /><br />
-                            I had previous experience as a frontend with an Argentine
-                            development team and as a programmer analyst for a technology
-                            innovation company.
+                            I have previous experience working as an IT Project Leader and
+                            Programmer Analyst in a technological innovation company and
+                            process optimization; proposing new solutions, and
+                            being part and cause of the fulfillment of the goals of each month
+                            of the company.
                             <br /><br />
                             I'm able to create web pages that are fast, efficient and intuitive,
                             adaptable to any device.
